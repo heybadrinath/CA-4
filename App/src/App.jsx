0b34ef10f0eca4d85e49e42d19750496004495f0
heybadrinath <ram.r@kalvium.community>
2024-01-20 15:07:13 +0530

    final commit and error handling

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,40 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import QuestionBox from './components/QuestionBox';
 import Result from './components/Result';
 
 function App() {
-  const [dark, setDark] = useState(false);
+  // getting mode from localStorage or setting the default to false
+  const [dark, setDark] = useState(localStorage.getItem('darkMode') === 'true' || false);
   const [index, setIndex] = useState(0);
   const [score, setScore] = useState(0);
 
-  // fucntion for increasing index
+  // Update localStorage when dark mode changes
+  useEffect(() => {
+    localStorage.setItem('darkMode', dark);
+  }, [dark]);
+
+  // Function for increasing index
   const changeIndex = () => setIndex(index + 1);
 
-  // function for changing the theme
+  // Function for changing the theme
   const changeTheme = () => setDark(!dark);
 
-  // function for reseting the index
+  // Function for resetting the index
   const resetIndex = () => setIndex(0);
 
   return (
-
     <div style={{ backgroundColor: dark ? "#000" : "#fff", color: dark ? "#fff" : "#000" }}>
-
       <div className="header">
-      
         <span className='kalvium' style={{ color: dark ? "#fff" : "#000" }}>KALVIUM</span>
-      
-        {/* button for changing dark theme and light theme */}
         <button className='mode' onClick={changeTheme} style={{ backgroundColor: dark ? "#fff" : "#000", color: dark ? "#000" : "#fff" }}>
           {dark ? "Light" : "Dark"}
         </button>
       </div>
-
       <div className='component'>
-
-        {/* redering questions componenet or result component based on the index value */}
-        {/* Conditional Rendering */}
+        {/* Rendering questions component or result component based on the index value */}
         {index < 5 ? (
           <QuestionBox index={index} changeIndex={changeIndex} dark={dark} score={score} changeScore={setScore} />
         ) : (
